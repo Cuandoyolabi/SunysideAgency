61da0ccb9214459115ffe939772e20a217e8e691
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-06-15 13:53:54 -0600

    Se vincula js al html y se crea el menu completo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-const headerHamburguer = document.getElementById("header-hamburguer-id");
+const headerHamburguer = document.getElementById('header-hamburguer-id');
+const menuDesplegable = document.getElementById('menu-desplegable');
 
+headerHamburguer.addEventListener('click', () => {
+    
+    menuDesplegable.classList.toggle('active');
+
+});
